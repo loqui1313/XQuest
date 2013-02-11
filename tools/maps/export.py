d1ceb63148d2ea10d15c550d962719575634c4e6
loqui1313 <Jgomez1319@hotmail.com>
2013-02-10 20:04:55 -0400

    Update tools/maps/export.py

--- tools/maps/export.py
+++ tools/maps/export.py
@@ -8,18 +8,18 @@ TEMP_FILE = SRC_FILE+'.json'
 
 mode = sys.argv[1] if len(sys.argv) > 1 else 'client'
 if mode == 'client':
-    DEST_FILE = '../../client/maps/world_client' # This will save two files (See exportmap.js)
+    DEST_FILE = '../../client/maps/world_client' # Esto le ahorrará dos archivos (Ver exportmap.js)
 else:
     DEST_FILE = '../../server/maps/world_server.json'
 
-# Convert the Tiled TMX file to a temporary JSON file
+# Convertir el archivo TMX Temporalmente a un archivo JSON
 print commands.getoutput('./tmx2json.py '+SRC_FILE+' '+TEMP_FILE)
 
-# Map exporting
+# Exportando Mapa
 print commands.getoutput('./exportmap.js '+TEMP_FILE+' '+DEST_FILE+' '+mode)
 
-# Remove temporary JSON file
+# Eliminar archivos temporales JSON
 print commands.getoutput('rm '+TEMP_FILE)
 
-# Send a Growl notification when the export process is complete
+# Enviar una notificación Growl cuando el proceso de exportación se ha completado
 print commands.getoutput('growlnotify --appIcon Tiled -name "Map export complete" -m "'+DEST_FILE+' was saved"')
